d19025463ee2340c8ed4d30a763789e5e5d842ee
potray <potrayarrick@gmail.com>
2015-05-05 18:33:16 +0200

    Quitando protección CRSF temporalmente dle registro

--- main_appsamblea/views.py
+++ main_appsamblea/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def main_page(request):
     return render(request, 'main_appsamblea/main_page.html')
 
+@csrf_exempt
 def registro(request):
     if request.method == 'POST':
         return HttpResponse(
