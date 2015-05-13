8e6c7c5160690e3be3d0512b99d63798e91480da
potray <potrayarrick@gmail.com>
2015-05-13 17:52:23 +0200

    Registro en Facebook, primera versión

--- main_appsamblea/views.py
+++ main_appsamblea/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+import json
+from main_appsamblea.models import Usuario
 
 
 def main_page(request):
@@ -9,9 +11,22 @@ def main_page(request):
 @csrf_exempt
 def registro(request):
     if request.method == 'POST':
+        objetoJSON = json.loads(request.body)
+        facebookID = objetoJSON['id']
+        nombre = objetoJSON['nombre']
+        apellidos = objetoJSON['apellidos']
+        email = objetoJSON['email']
+
+        nuevoUsuario = Usuario(username=facebookID, first_name=nombre, last_name=apellidos, email=email, password=facebookID)
+        if nuevoUsuario.isOk():
+            nuevoUsuario.save()
+            mensaje = 'ok'
+        else:
+            mensaje = 'error'
+
+
         return HttpResponse(
-            request,
-            'hola'
+            mensaje
         )
     else:
         return render(request, 'main_appsamblea/main_page.html')
