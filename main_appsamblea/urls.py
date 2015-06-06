8dd9c309b991c93483e212fd86ef7b670a06dd92
carltorres <togacharls@gmail.com>
2015-06-06 19:01:29 +0200

    Otra subida por el dichoso "Bug"

--- main_appsamblea/urls.py
+++ main_appsamblea/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import *
 import views
 
-from django.db.models.loading import cache as model_cache
-if not model_cache.loaded:
-    model_cache.get_models()
-
 urlpatterns = patterns('',
                        (r'^$', views.main_page),
                        (r'registro', views.registro),
